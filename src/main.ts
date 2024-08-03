7f76fe84b4235361e248412d6f2fcb65aaa8454e
pabloacisera <pabloacisera2021@gmail.com>
2024-08-03 11:33:29 -0300

    udpate

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv'
+
+
+dotenv.config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:4200', // Origen permitido
+    origin: process.env.FRONTEND_URL, // Origen permitido
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     credentials: true, // Permitir credenciales (cookies, headers de autenticación, etc.)
   };
